38d0b3317e88010b18baed6bc1e4c8fb78da404d
zhouliliang <zhouliliang@kgdianzi.com>
2019-09-16 11:44:46 +0800

    修改sql脚本

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `user` (
-                        `id` bigint(20) auto_increment NOT NULL COMMENT '主键ID',
+create schema if not exists `mybatisplus`;
+
+CREATE TABLE `mybatisplus`.`user` (
+                        `id` bigint(20) auto_increment NOT NULL COMMENT '主键ID' primary key,
                         `name` varchar(30) DEFAULT NULL COMMENT '姓名',
                         `age` int(11) DEFAULT NULL COMMENT '年龄',
-                        `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
-                        PRIMARY KEY (`id`)
+                        `email` varchar(50) DEFAULT NULL COMMENT '邮箱'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
